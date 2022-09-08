156c4f1f889b0ebb639915700807f6d6eda7b063
trustpizza <maxelolsson@gmail.com>
2022-09-07 19:45:53 -0500

    Add start_game functionality

--- script.rb
+++ script.rb
@@ -1,16 +1,16 @@
 class Player
-    attr_accessor :name, :game_piece,
+    attr_accessor :name, :game_piece
+    @@player_counter = 0
     def initialize(name, game_piece)
         @name = name
         @game_piece = game_piece    
     end
-    @@player_counter = 0
     def self.create_player
         @@player_counter += 1
-        puts "Hi Player #{@@player_counter}, What is your name?";
-        name = gets.chomp;
-        puts "#{name}, what letter would you like to represent you?";
-        game_piece = gets.chomp;
+        puts "Hi Player #{@@player_counter}, What is your name?"
+        name = gets.chomp
+        puts "#{name}, what letter would you like to represent you?"
+        game_piece = gets.chomp
         Player.new(name, game_piece)
     end
     def start_game
